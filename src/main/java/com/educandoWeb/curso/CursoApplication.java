da138a8aa9c63500b04198ba20dd006c3b1ced3d
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-05-12 19:13:27 -0300

    add comment

--- src/main/java/com/educandoWeb/curso/CursoApplication.java
+++ src/main/java/com/educandoWeb/curso/CursoApplication.java
@@ -3,6 +3,10 @@ package com.educandoWeb.curso;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// JPA = Ferramenta de mapeamento objeto relacional padrão do Java,
+// ela faz uma conversão entre o modelo orientado a objetos e o paradigma relacional
+// do banco de dados relacional
+
 @SpringBootApplication
 public class CursoApplication {
 
