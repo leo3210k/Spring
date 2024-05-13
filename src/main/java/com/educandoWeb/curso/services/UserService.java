eb7072626489cb328ee802cd960e04d48126b271
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-05-13 11:22:49 -0300

    add findAll and findById methods

---
+++ src/main/java/com/educandoWeb/curso/services/UserService.java
@@ -0,0 +1,29 @@
+package com.educandoWeb.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoWeb.curso.entities.User;
+import com.educandoWeb.curso.repositories.UserRepository;
+
+// Poderia ser @Component, que também registra a classe como um componente do Spring,
+// podendo ser injetado automaticamente com o @Autowired, mas o @Service é utilizado
+// para ficar mais semanticamente específico
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
