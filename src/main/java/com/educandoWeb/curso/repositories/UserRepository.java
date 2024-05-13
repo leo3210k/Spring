c34c555f284b2de212b361478f5220e3a291176a
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-05-13 11:21:36 -0300

    add comment

--- src/main/java/com/educandoWeb/curso/repositories/UserRepository.java
+++ src/main/java/com/educandoWeb/curso/repositories/UserRepository.java
@@ -7,6 +7,10 @@ import com.educandoWeb.curso.entities.User;
 // Vai ser o repository responsável por fazer operações com a entidade User
 // Não precisa criar a implementação da interface, porque o spring-data-jpa
 // ja tem uma implementação padrão para essa interface
+// Classe que salva os dados no banco
+// Não é necessário utilizar @Repository (como o @Component),
+// porque a classe ja está herdando do JpaRepository que já
+// está registrado como componente do Spring
 public interface UserRepository extends JpaRepository<User, Long>{
 
 }
