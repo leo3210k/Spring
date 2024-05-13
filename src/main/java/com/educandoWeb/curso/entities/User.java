23809ca32bd9c96ff6c17a05b6ec6c670cfa47f4
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-05-13 11:45:25 -0300

    add Order field

--- src/main/java/com/educandoWeb/curso/entities/User.java
+++ src/main/java/com/educandoWeb/curso/entities/User.java
@@ -1,12 +1,15 @@
 package com.educandoWeb.curso.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 // Interface chamada Serializable
@@ -26,6 +29,9 @@ public class User implements Serializable {
 	private String phone;
 	private String password;
 	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+
 	public User() {
 	}
 
@@ -76,6 +82,10 @@ public class User implements Serializable {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
