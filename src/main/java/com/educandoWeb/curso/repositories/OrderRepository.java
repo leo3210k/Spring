8fa89b91f9eb39b181330103175a7ba2f046de21
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-05-14 16:48:00 -0300

    create order repository

---
+++ src/main/java/com/educandoWeb/curso/repositories/OrderRepository.java
@@ -0,0 +1,16 @@
+package com.educandoWeb.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educandoWeb.curso.entities.Order;
+
+// Vai ser o repository responsável por fazer operações com a entidade User
+// Não precisa criar a implementação da interface, porque o spring-data-jpa
+// ja tem uma implementação padrão para essa interface
+// Classe que salva os dados no banco
+// Não é necessário utilizar @Repository (como o @Component),
+// porque a classe ja está herdando do JpaRepository que já
+// está registrado como componente do Spring
+public interface OrderRepository extends JpaRepository<Order, Long>{
+
+}
