d6d836b8f47b1dc8ad97cd42c0817142e706dd7f
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-05-16 16:49:59 -0300

    add json ignore

--- src/main/java/com/educandoWeb/curso/entities/User.java
+++ src/main/java/com/educandoWeb/curso/entities/User.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -29,6 +31,9 @@ public class User implements Serializable {
 	private String phone;
 	private String password;
 	
+	// Lazy loading = ao se ter uma associação "To many", o JPA não carrega
+	// os objetos "para muitos", para não estourar a memória do computador
+	@JsonIgnore
 	@OneToMany(mappedBy = "client")
 	private List<Order> orders = new ArrayList<>();
 
