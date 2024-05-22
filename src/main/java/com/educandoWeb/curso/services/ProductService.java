cc5b6dfd91c7a112f1ad3d2d008a2eb460a9a930
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-05-22 16:59:37 -0300

    create product service

---
+++ src/main/java/com/educandoWeb/curso/services/ProductService.java
@@ -0,0 +1,29 @@
+package com.educandoWeb.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoWeb.curso.entities.Product;
+import com.educandoWeb.curso.repositories.ProductRepository;
+
+// Poderia ser @Component, que também registra a classe como um componente do Spring,
+// podendo ser injetado automaticamente com o @Autowired, mas o @Service é utilizado
+// para ficar mais semanticamente específico
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}
