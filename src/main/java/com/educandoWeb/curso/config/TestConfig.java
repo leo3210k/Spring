3ca66a4b90b8881bb0c882b77a44e626fe373969
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-05-22 13:36:18 -0300

    add category database seeding

--- src/main/java/com/educandoWeb/curso/config/TestConfig.java
+++ src/main/java/com/educandoWeb/curso/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.educandoWeb.curso.entities.Category;
 import com.educandoWeb.curso.entities.Order;
 import com.educandoWeb.curso.entities.User;
 import com.educandoWeb.curso.entities.enums.OrderStatus;
+import com.educandoWeb.curso.repositories.CategoryRepository;
 import com.educandoWeb.curso.repositories.OrderRepository;
 import com.educandoWeb.curso.repositories.UserRepository;
 
@@ -29,9 +31,16 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers"); 
+		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
 		
@@ -41,5 +50,6 @@ public class TestConfig implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 	}
 }
