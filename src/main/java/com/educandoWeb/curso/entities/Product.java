7ffa3bc702451096bfffa8e35d8cfb453d0097be
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-05-22 17:20:40 -0300

    add join table product category

--- src/main/java/com/educandoWeb/curso/entities/Product.java
+++ src/main/java/com/educandoWeb/curso/entities/Product.java
@@ -9,8 +9,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_product")
@@ -29,7 +31,10 @@ public class Product implements Serializable {
 	// Instanciamos para que a coleção não comece valendo nula
 	// ela tem que começar vazia mas instanciada
 	// Porque HashSet? Set é uma interface, não pode ser instanciada
-	@Transient
+	@ManyToMany
+	@JoinTable(name = "tb_product_category",
+	joinColumns = @JoinColumn(name = "product_id"),
+	inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
 	
 	public Product() {
