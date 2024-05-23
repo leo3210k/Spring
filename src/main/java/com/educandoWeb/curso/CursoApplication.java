e1f351fe41524783f76d602550c665e5e724281d
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-05-23 18:43:51 -0300

    add classes instantiation

--- src/main/java/com/educandoWeb/curso/CursoApplication.java
+++ src/main/java/com/educandoWeb/curso/CursoApplication.java
@@ -1,5 +1,18 @@
 package com.educandoWeb.curso;
 
+import com.educandoWeb.curso.repositories.impl.UserRepositoryImpl;
+import com.educandoWeb.curso.resources.CategoryResource;
+import com.educandoWeb.curso.resources.OrderResource;
+import com.educandoWeb.curso.resources.ProductResource;
+import com.educandoWeb.curso.resources.UserResource;
+import com.educandoWeb.curso.resources.impl.CategoryResourceImpl;
+import com.educandoWeb.curso.resources.impl.OrderResourceImpl;
+import com.educandoWeb.curso.resources.impl.ProductResourceImpl;
+import com.educandoWeb.curso.resources.impl.UserResourceImpl;
+import com.educandoWeb.curso.services.impl.CategoryServiceImpl;
+import com.educandoWeb.curso.services.impl.OrderServiceImpl;
+import com.educandoWeb.curso.services.impl.ProductServiceImpl;
+import com.educandoWeb.curso.services.impl.UserServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,6 +24,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CursoApplication {
 
 	public static void main(String[] args) {
+		UserResource user = new UserResourceImpl(new UserServiceImpl(new UserRepositoryImpl()));
+		ProductResource product = new ProductResourceImpl(new ProductServiceImpl());
+		OrderResource order = new OrderResourceImpl(new OrderServiceImpl());
+		CategoryResource category = new CategoryResourceImpl(new CategoryServiceImpl());
+
 		SpringApplication.run(CursoApplication.class, args);
 	}
 
