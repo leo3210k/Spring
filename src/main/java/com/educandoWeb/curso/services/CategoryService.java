4feca36502ab42856ad0105a33472cea873d8364
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-05-23 18:41:48 -0300

    create services interfaces

--- src/main/java/com/educandoWeb/curso/services/CategoryService.java
+++ src/main/java/com/educandoWeb/curso/services/CategoryService.java
@@ -1,29 +1,11 @@
 package com.educandoWeb.curso.services;
 
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.educandoWeb.curso.entities.Category;
-import com.educandoWeb.curso.repositories.CategoryRepository;
 
-// Poderia ser @Component, que também registra a classe como um componente do Spring,
-// podendo ser injetado automaticamente com o @Autowired, mas o @Service é utilizado
-// para ficar mais semanticamente específico
-@Service
-public class CategoryService {
-	
-	@Autowired
-	private CategoryRepository repository;
-	
-	public List<Category> findAll(){
-		return repository.findAll();
-	}
-	
-	public Category findById(Long id) {
-		Optional<Category> obj = repository.findById(id);
-		return obj.get();
-	}
+import java.util.List;
+
+public interface CategoryService {
+    public List<Category> findAll();
+
+    public Category findById(Long id);
 }
--- src/main/java/com/educandoWeb/curso/services/UserService.java
+++ src/main/java/com/educandoWeb/curso/services/UserService.java
@@ -1,29 +1,13 @@
 package com.educandoWeb.curso.services;
 
+import com.educandoWeb.curso.entities.User;
+
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+public interface UserService {
 
-import com.educandoWeb.curso.entities.User;
-import com.educandoWeb.curso.repositories.UserRepository;
+    public List<User> findAll();
 
-// Poderia ser @Component, que também registra a classe como um componente do Spring,
-// podendo ser injetado automaticamente com o @Autowired, mas o @Service é utilizado
-// para ficar mais semanticamente específico
-@Service
-public class UserService {
-	
-	@Autowired
-	private UserRepository repository;
-	
-	public List<User> findAll(){
-		return repository.findAll();
-	}
-	
-	public User findById(Long id) {
-		Optional<User> obj = repository.findById(id);
-		return obj.get();
-	}
+    public User findById(Long id);
 }
