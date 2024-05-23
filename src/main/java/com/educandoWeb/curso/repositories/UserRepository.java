f1194b84280d85915f1024417c187d31e4384680
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-05-23 18:40:11 -0300

    create own user repository interface

--- src/main/java/com/educandoWeb/curso/repositories/UserRepository.java
+++ src/main/java/com/educandoWeb/curso/repositories/UserRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.educandoWeb.curso.entities.User;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 // Vai ser o repository responsável por fazer operações com a entidade User
 // Não precisa criar a implementação da interface, porque o spring-data-jpa
 // ja tem uma implementação padrão para essa interface
@@ -11,6 +15,7 @@ import com.educandoWeb.curso.entities.User;
 // Não é necessário utilizar @Repository (como o @Component),
 // porque a classe ja está herdando do JpaRepository que já
 // está registrado como componente do Spring
-public interface UserRepository extends JpaRepository<User, Long>{
-
+public interface UserRepository {
+    List<User> findAll();
+    Optional<User> findById(Long id);
 }
